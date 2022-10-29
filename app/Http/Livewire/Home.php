34bfea02d25a399120b4703828e6aa2854a18ca0
Kenet Picado <83382624+kenetpicado@users.noreply.github.com>
2022-10-29 13:03:46 -0600

    home

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Home extends Component
@@ -10,6 +11,30 @@ class Home extends Component
 
     public function render()
     {
-        return view('livewire.home');
+        $cursos = DB::table('cursos')
+            ->where('estado', 1)
+            ->count();
+
+        $docentes = DB::table('docentes')
+            ->where('estado', 1)
+            ->count();
+
+        $alumnos = DB::table('alumnos')
+            ->where('created_at', '>=', date('Y-m-d'))
+            ->count();
+
+        $grupos = DB::table('grupos')
+            ->where('estado', 1)
+            ->where('anyo', date('Y'))
+            ->count();
+
+        $info = (object) [
+            'cursos' => $cursos,
+            'docentes' => $docentes,
+            'alumnos' => $alumnos,
+            'grupos' => $grupos
+        ];
+
+        return view('livewire.home', compact('info'));
     }
 }
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,8 +1,20 @@
-<div class="col-lg-4 col-md-12 mb-2">
+@props(['label', 'value', 'icon', 'subtitle' => 'Activos'])
+<div class="col-lg-3 col-sm-12">
     <div class="card">
         <div class="card-body">
             <div class="d-flex justify-content-between p-md-1">
-                {{ $slot }}
+                <div class="d-flex flex-row">
+                    <div class="align-self-center">
+                        <h2 class="h2 mb-0 me-4 fw-bolder">{{ $value }}</h2>
+                    </div>
+                    <div>
+                        <h4 class="fw-bolder">{{ $label }}</h4>
+                        <p class="mb-0">{{ $subtitle }}</p>
+                    </div>
+                </div>
+                <div class="align-self-center">
+                    <i class="fas fa-{{ $icon }} text-black-50 fa-2x opacity-10"></i>
+                </div>
             </div>
         </div>
     </div>
